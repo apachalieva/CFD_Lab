ceef4c0155d6fd5c98199f74f190ac908be1115f
Davide D'Angella <da.dange@tiscalinet.it>
2014-05-03 12:53:01 +0200

    Signed-off-by: Davide D'Angella <da.dange@tiscalinet.it>

--- Worksheet2/framework_lbm/main.c
+++ Worksheet2/framework_lbm/main.c
@@ -6,13 +6,46 @@
 #include "initLB.h"
 #include "visualLB.h"
 #include "boundary.h"
-
-int main(int argc, char *argv[]){
-  /* TODO */
+#include "LBDefinitions.h"
 
 
-  return 0;
+
+int main (int argc, char *argv[]){
+	double *collideField=NULL, *streamField=NULL, tau, velocityWall[3];
+	int t, xlength, timesteps, timestepsPerPlotting, *flagField=NULL;
+
+	readParameters( &xlength, &tau, velocityWall, &timesteps, &timestepsPerPlotting, argc, argv);
+
+	collideField = malloc(Q * CUBE(xlength+2) * sizeof(double));
+	streamField  = malloc(Q * CUBE(xlength+2) * sizeof(double));
+	flagField    = malloc(    CUBE(xlength+2) * sizeof(double));
+
+
+	initialiseFields(collideField, streamField, flagField, xlength);
+
+	for(t=0; t < timesteps; t++){
+		double *swap=NULL;
+
+		doStreaming(collideField, streamField, flagField, xlength);
+
+		swap = collideField;
+		collideField = streamField;
+		streamField = swap;
+
+		doCollision(collideField,flagField,&tau,xlength);
+		treatBoundary(collideField,flagField,velocityWall,xlength);
+
+		if (t%timestepsPerPlotting==0){
+			writeVtkOutput(collideField, flagField, argv[1], t, xlength);
+		}
+	}
+
+	return 0;
 }
+
+
+
+
 
 #endif
 
