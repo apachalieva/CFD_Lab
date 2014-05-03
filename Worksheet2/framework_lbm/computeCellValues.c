471e1a0bf92515bf3ecab3a1137c904fb5555c8d
Davide D'Angella <da.dange@tiscalinet.it>
2014-05-03 12:51:00 +0200

    implemented computeCellValues.c
    Signed-off-by: Davide D'Angella <da.dange@tiscalinet.it>

--- Worksheet2/framework_lbm/computeCellValues.c
+++ Worksheet2/framework_lbm/computeCellValues.c
@@ -1,14 +1,34 @@
 #include "computeCellValues.h"
+#include "LBDefinitions.h"
+#include "helper.h"
 
 void computeDensity(const double *const currentCell, double *density){
-  /* TODO */
+	unsigned long i;
+
+	*density=.0;
+	for(i=0; i<Q; i++)
+		*density = *density + currentCell[i];
 }
 
 void computeVelocity(const double * const currentCell, const double * const density, double *velocity){
-  /* TODO */
+	unsigned long i, d;
+
+	for(d=0ul; d<DIM; d++)
+		velocity[d] = .0;
+
+	for(i=0ul; i<Q; i++)
+		for(d=0ul; d<DIM; d++)
+			velocity[d] += currentCell[i] * (double)LATTICEVELOCITIES[i][d] / *density;
 }
 
 void computeFeq(const double * const density, const double * const velocity, double *feq){
- /* TODO */
+	unsigned long i;
+
+	for(i=0ul; i<Q; i++)
+		feq[i] = LATTICEWEIGHTS[i] * (*density) * (1.0 + DOTP(LATTICEVELOCITIES[i], velocity) / SQ(C_S)
+			+ SQ( DOTP(LATTICEVELOCITIES[i], velocity) ) / ( 2.0 * SQ(SQ(C_S)) )
+			- DOTP(velocity, velocity) / ( 2.0 * SQ(C_S) ));
 }
+
+
 
