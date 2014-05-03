9a36df3642d987afea97aa2747e5f964c08b5df7
Davide D'Angella <da.dange@tiscalinet.it>
2014-05-03 12:49:31 +0200

    specified constants for model D3Q19

--- Worksheet2/framework_lbm/LBDefinitions.h
+++ Worksheet2/framework_lbm/LBDefinitions.h
@@ -1,9 +1,12 @@
 #ifndef _LBDEFINITIONS_H_
 #define _LBDEFINITIONS_H_
 
-  static const int LATTICEVELOCITIES[19][3];
-  static const double LATTICEWEIGHTS[19];
-  static const double C_S;
+#define Q 19
+#define DIM 3
+
+  static const int LATTICEVELOCITIES[19][3] = {   {0,-1,-1}, {-1,0,-1}, {0,0,-1}, {1,0,-1}, {0,1,-1}, {-1,-1,0}, {0,-1,0}, {1,-1,0}, {-1,0,0}, {0,0,0}, {1,0,0}, {-1,1,0}, {0,1,0}, {1,1,0}, {0,-1,1}, {-1,0,1}, {0,0,1}, {1,0,1}, {0,1,1} };
+  static const double LATTICEWEIGHTS[19] = { 1.0/36.0, 1.0/36.0, 2.0/36.0, 1.0/36.0, 1.0/36.0, 1.0/36.0, 2.0/36.0, 1.0/36.0, 2.0/36.0, 12.0/36.0,  2.0/36.0,  1.0/36.0,  2.0/36.0,  1.0/36.0, 1.0/36.0,  1.0/36.0,  2.0/36.0,  1.0/36.0,  1.0/36.0 };
+  static const double C_S = 0.577350269; /* 1/sqrt(3) */
 
 #endif
 
