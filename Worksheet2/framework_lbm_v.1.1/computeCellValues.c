7dfa5a7ff44b1466e2d81a575491ecaa18662c41
davidedangella <da.dange@tiscalinet.it>
2014-05-05 20:52:22 +0200

    Update computeCellValues.c

--- Worksheet2/framework_lbm_v.1.1/computeCellValues.c
+++ Worksheet2/framework_lbm_v.1.1/computeCellValues.c
@@ -3,7 +3,7 @@
 #include "helper.h"
 
 void computeDensity(const double *const currentCell, double *density){
-	unsigned long i;
+	int i;
 
 	*density=.0;
 	for(i=0; i<Q; i++)
@@ -11,20 +11,23 @@ void computeDensity(const double *const currentCell, double *density){
 }
 
 void computeVelocity(const double * const currentCell, const double * const density, double *velocity){
-	unsigned long i, d;
+	int i, d;
 
-	for(d=0ul; d<DIM; d++)
+	for(d=0; d<DIM; d++)
 		velocity[d] = .0;
 
-	for(i=0ul; i<Q; i++)
-		for(d=0ul; d<DIM; d++)
-			velocity[d] += currentCell[i] * (double)LATTICEVELOCITIES[i][d] / *density;
+	for(i=0; i<Q; i++)
+		for(d=0; d<DIM; d++)
+			velocity[d] += currentCell[i] * (double)LATTICEVELOCITIES[i][d];
+			
+	for(d=0; d<DIM; d++)
+		velocity[d] /= (*density);
 }
 
 void computeFeq(const double * const density, const double * const velocity, double *feq){
 	unsigned long i;
 
-	for(i=0ul; i<Q; i++)
+	for(i=0; i<Q; i++)
 		feq[i] = LATTICEWEIGHTS[i] * (*density) * (1.0 + DOTP(LATTICEVELOCITIES[i], velocity) / SQ(C_S)
 			+ SQ( DOTP(LATTICEVELOCITIES[i], velocity) ) / ( 2.0 * SQ(SQ(C_S)) )
 			- DOTP(velocity, velocity) / ( 2.0 * SQ(C_S) ));
