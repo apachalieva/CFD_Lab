110556c26f2a9334093a1defa4e1dfb9aaee57b1
apachalieva <apachalieva@gmail.com>
2014-05-05 21:00:08 +0200

    Update streaming.c

--- Worksheet2/framework_lbm_v.1.1/streaming.c
+++ Worksheet2/framework_lbm_v.1.1/streaming.c
@@ -6,15 +6,14 @@
 void doStreaming( double *collideField, double *streamField, int *flagField, int xlength ){
  
   /* 
-   *This worksheet is restricted to the D3Q19 model - three-dimentional model with 19 lattice velocities ci 
+   * This worksheet is restricted to the D3Q19 model - three-dimentional model with 19 lattice velocities ci 
    */		
-    int x, y, z, i;			/* Helper variables for moving through the velocities in our 3D model */
+    int x, y, z, i;			 /* Helper variables for moving through the velocities in our 3D model */
     
     for( z = 1; z < xlength + 1; ++z ){
       for( y = 1; y < xlength + 1; ++y ){
 	for( x = 1; x < xlength + 1; ++x ){
 	  /* Proceed, only if the state of the cells is FLUID = 0 */
-	  
 	  if( flagField[ x + (xlength+2) * y + SQ(xlength+2) * z ] == 0 ){
 	   for( i = 0; i < Q; ++i ){
 	     streamField[ Q * ( x + (xlength+2) * y + SQ(xlength+2) * z ) + i ] = collideField[ Q * ( ( x - LATTICEVELOCITIES[i][0] ) + 
