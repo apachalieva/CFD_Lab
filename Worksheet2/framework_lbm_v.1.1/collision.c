5dfbf547b75e77cf74fa85bb883eaa430467c9b6
davidedangella <da.dange@tiscalinet.it>
2014-05-05 17:19:40 +0200

    Update collision.c

--- Worksheet2/framework_lbm_v.1.1/collision.c
+++ Worksheet2/framework_lbm_v.1.1/collision.c
@@ -7,15 +7,15 @@ void computePostCollisionDistributions(double *currentCell, const double * const
 	unsigned long i;
 
 	for(i=0ul; i<Q; i++)
-		currentCell[i] = currentCell[i] + (feq[i] - currentCell[i]) / *tau;
+		currentCell[i] = currentCell[i] + (feq[i] - currentCell[i]) / (*tau);
 }
 
 void doCollision(double *collideField, int *flagField,const double * const tau,int xlength){
 	double density, velocity[3], feq[Q], *currentCell;
-	unsigned long x, y, z, i;
+	unsigned long x, y, z;
 
-	for(z=1ul; z<xlength+1; z++){
-		for(y=1ul; y<xlength+1; y++){
+	for(z=1ul; z<xlength+1; z++)
+		for(y=1ul; y<xlength+1; y++)
 			for(x=1ul; x<xlength+1; x++){
 				/* get the current cell */
 				currentCell = collideField + Q * ( x + (xlength+2) * y + SQ(xlength+2) * z );
@@ -25,6 +25,5 @@ void doCollision(double *collideField, int *flagField,const double * const tau,i
 				computeVelocity( currentCell, &density, velocity );
 				computeFeq( &density, velocity,feq );
 				computePostCollisionDistributions( currentCell, tau, feq );
-			}
+			    }
 }
-
