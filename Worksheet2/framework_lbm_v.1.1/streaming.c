4a8951e44943cb9d4a15f1973c925d5a9f5ef8ae
davidedangella <da.dange@tiscalinet.it>
2014-05-05 20:55:05 +0200

    Update streaming.c

--- Worksheet2/framework_lbm_v.1.1/streaming.c
+++ Worksheet2/framework_lbm_v.1.1/streaming.c
@@ -8,15 +8,15 @@ void doStreaming( double *collideField, double *streamField, int *flagField, int
   /* 
    *This worksheet is restricted to the D3Q19 model - three-dimentional model with 19 lattice velocities ci 
    */		
-    unsigned long x, y, z, i;			/* Helper variables for moving through the velocities in our 3D model */
+    int x, y, z, i;			/* Helper variables for moving through the velocities in our 3D model */
     
-    for( z = 1lu; z < xlength + 1; ++z ){
-      for( y = 1lu; y < xlength + 1; ++y ){
-	for( x = 1lu; x < xlength + 1; ++x ){
+    for( z = 1; z < xlength + 1; ++z ){
+      for( y = 1; y < xlength + 1; ++y ){
+	for( x = 1; x < xlength + 1; ++x ){
 	  /* Proceed, only if the state of the cells is FLUID = 0 */
 	  
 	  if( flagField[ x + (xlength+2) * y + SQ(xlength+2) * z ] == 0 ){
-	   for( i = 0lu; i < Q; ++i ){
+	   for( i = 0; i < Q; ++i ){
 	     streamField[ Q * ( x + (xlength+2) * y + SQ(xlength+2) * z ) + i ] = collideField[ Q * ( ( x - LATTICEVELOCITIES[i][0] ) + 
 												  (xlength+2) * ( y - LATTICEVELOCITIES[i][1] ) + 
 												  SQ(xlength+2) * ( z - LATTICEVELOCITIES[i][2] ) ) + 
