23dfb56adc3ce78172147d756863a3fae9bba5b9
workstation <apachalieva@gmail.com>
2014-05-17 17:40:51 +0200

    added bc for obstacle

--- Worksheet3/boundary_val.h
+++ Worksheet3/boundary_val.h
@@ -10,12 +10,13 @@ void boundaryvalues(
   int jmax,
   double **U,
   double **V,
-  int *b		/* vector representing different types of boundaries */
+  int *boundrs,		/* vector representing different types of boundaries */
+  int **Flag		/* Flag field, describing the geometry of the problem */
 );
 
 /* fuction for
  * INFLOW boundary condition
  */
-void spec_boundary_val( char* problem, int imax, int jmax, double **U, double **V, double **P, double u_in, double v_in, double dp);
+void spec_boundary_val( char* problem, int imax, int jmax,double **U, double **V, double Re, double dp, double h);
 
 #endif
